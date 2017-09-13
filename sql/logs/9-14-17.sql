962ce58acddc23a04f7336ea7a53e96eba398292
jethro <jethro@kayweb.com.au>
2017-09-14 03:24:26 +0800

    sql logs

--- sql/logs/9-14-17.sql
+++ sql/logs/9-14-17.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `customer` ADD `is_verified` TINYINT(1) NOT NULL DEFAULT '1' AFTER `enabled`;
+
+ALTER TABLE `customer` ADD `web_code_valid` TINYINT(1) NOT NULL DEFAULT '1' AFTER `web_code`;
